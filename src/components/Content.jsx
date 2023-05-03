fdeaa285641f676a109dae64a2587b23d1f08194
adri006 <adrixan.mmairambekov@gmail.com>
2023-05-03 03:22:36 +0300

    feat: created component Filtration

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import Header from "./Header";
 
 const Content = () => {
-    return(
-        <Header/>
+    return (
+        <>
+            <Header />
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" htmlFor="exampleCheck1">
+                        Check me out
+                    </label>
+                </div>
+                
+        </>
     )
 }
 export default Content;
